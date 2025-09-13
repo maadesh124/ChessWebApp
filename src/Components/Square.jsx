f8a439c3640386e4aa25217ca8cccb2340a9b77f
Maadesh A <maadeshashok124@gmail.com>
2025-09-13 18:21:18 +0530

    piece file path resolved

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -6,7 +6,7 @@ export const Square = ({ globalRef, pos }) => {
   const size = globalRef.current.boardSize / 8;
   const boardState = globalRef.current.board.getBoardState();
   const piece = boardState[pos[0]][pos[1]];
-  const path = `public/assets/${piece}.svg`;
+  const path = `/assets/${piece}.svg`;
   let color;
   if (pos[0] % 2 === pos[1] % 2) color = `bg-gray-600`;
   else color = `bg-gray-200`;
